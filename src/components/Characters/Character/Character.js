1095be4fe28ace089594f81c55169449e8dcd58c
Davis Wambugu <dwambugu@cellulant.com>
2021-06-20 14:12:40 +0300

    animate character recors on hover

--- src/components/Characters/Character/Character.js
+++ src/components/Characters/Character/Character.js
@@ -6,24 +6,25 @@ const Character = (props) => {
   const showStatus = (status) => {
     if (status === 'Alive') {
       return (
-        <span className="py-1 px-6 text-green-400 bg-green-100 rounded-lg capitalize">{character.status}</span>
+        <span className="py-1 px-6 text-green-400 capitalize bg-green-100 rounded-lg">{character.status}</span>
       )
     } else if (status === 'Dead') {
       return (
-        <span className="py-1 px-6 text-red-400 bg-red-100 rounded-lg capitalize">{character.status}</span>
+        <span className="py-1 px-6 text-red-400 capitalize bg-red-100 rounded-lg">{character.status}</span>
       )
     } else if (status === 'unknown') {
       return (
-        <span className="py-1 px-6 text-yellow-400 bg-yellow-100 rounded-lg capitalize">{character.status}</span>
+        <span className="py-1 px-6 text-yellow-400 capitalize bg-yellow-100 rounded-lg">{character.status}</span>
       )
     }
   }
   return (
     <div className="flex flex-col">
-      <div className="grid grid-cols-6 py-4 px-6 text-gray-400 hover:cursor-pointer">
+      <div
+        className="group grid grid-cols-6 py-3 group-hover:py-0 px-6 text-gray-400 rounded-lg transition duration-150 ease-in transform cursor-pointer hover:px-10 hover:bg-white hover:shadow-xl hover:scale-105">
         <div className="flex flex-row col-span-3 items-center -ml-6">
           <img src={character.image} alt=""
-               className="object-cover object-center w-16 h-16 rounded-lg"/>
+               className="object-cover object-center w-16 h-16 rounded-lg group-hover:h-32 group-hover:w-24"/>
           <div className="ml-4">
             <p className="font-semibold text-gray-600">{character.name}</p>
             <p className="mt-1 text-gray-400">Origin: {character.origin.name}</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      height: ['hover', 'focus', 'group-hover'],
+      width: ['hover', 'focus', 'group-hover'],
+      margin: ['hover', 'focus', 'group-hover'],
+      padding: ['hover', 'focus', 'group-hover'],
+    },
   },
   plugins: [],
 }
